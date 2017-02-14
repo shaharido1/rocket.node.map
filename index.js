5d2e12b6ceaf5e9d4983299105997e7d71be86f7
Ido <shaharido1@gmail.com>
2017-02-14 16:49:15 +0200

    api to rocket

--- index.js
+++ index.js
@@ -14,14 +14,13 @@ app.post('/', function (req, res) {
     res.send('in post');
     console.log("in post");
     console.log(JSON.stringify(req));
-    request.POST(
+    request.post(
         {url: "http://localhost:3000/api/v1/login",
          form : {user: "ido@webiks.com", password: "Mangosos1!"}},
          function(err,httpResponse,body){
              console.log(body)
          })
 });
-
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!')
 });
